66b98119eac25fe916c448d760c1cadd8f8b1766
Ivan Yazeth Rodriguez Hernandez <21011713@itsoeh.edu.mx>
2024-06-25 11:50:48 -0600

    Modifique archivos

--- Ejercicios/Eje12b.py
+++ Ejercicios/Eje12b.py
@@ -2,13 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def ojiva_calorías(datos):
+
+def ojiva_calorías(datosb):
   
-    sorted_datos = sorted(datos)
+    sorted_datos = sorted(datosb)
     cumulative_freq = [sum(sorted_datos[:i+1]) for i in range(len(sorted_datos))]
     plt.figure(figsize=(10,6))
-    plt.plot(sorted_datos, cumulative_freq)
+    plt.plot(sorted_datos, cumulative_freq, marker = 'o')
     plt.xlabel('Calorías')
     plt.ylabel('Frecuencia acumulada')
     plt.title('Ojiva de Calorías en Sándwiches')
+    plt.grid()
     plt.show()
+
