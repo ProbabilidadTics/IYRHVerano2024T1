cb60708ccad2e3b2ff4aa847cf7b17e011f47171
Ivan Yazeth Rodriguez Hernandez <21011713@itsoeh.edu.mx>
2024-06-27 09:10:54 -0600

    Ejercicio 8, sección a

---
+++ Ejercicios/Eje8.py
@@ -0,0 +1,12 @@
+import Ejercicios.Graficas as graf 
+import Ejercicios.Tabla as tbl
+import Ejercicios.DigDisyCua as disCua
+
+
+datosDecimal = [
+    2.5,4.5,5.8,2.2,5.7,5.3,4.0,0.9,2.5,2.3,3.2,4.9,2.7,3.7,3.8,4.9,2.8,3.6,4.2,3.1,7.2,1.9,5.9,3.3,2.9,1.1,1.7,2.6,3.1,4.4
+]
+
+print("A. Son datos continuos")
+
+
