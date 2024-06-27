ce9551ab37b10544aee8c286535ee73c03f0b2d3
Ivan Yazeth Rodriguez Hernandez <21011713@itsoeh.edu.mx>
2024-06-27 09:35:47 -0600

    Se modifico el ojiva del Ejercicio 12

--- Ejercicios/Eje12.py
+++ Ejercicios/Eje12.py
@@ -1,11 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import Ejercicios.Graficas as graf 
+import Ejercicios.Tabla as tbl
+import Ejercicios.DigCont as digCont
 
-def histograma_calorías(datos, num_clases=6):
-  
-    plt.figure(figsize=(10,6))
-    plt.hist(datos, bins=num_clases, edgecolor='black')
-    plt.xlabel('Calorías')
-    plt.ylabel('Frecuencia')
-    plt.title('Histograma de Calorías en Sándwiches')
-    plt.show()
+datosEj12 = [
+    250,790,470,510,500,330,300,750,580,740,420,260,440,770,400,540,530,330,390,380,340,460,530,260,410,360,270,510,630,330
+]
+print(len(datosEj12))
+
+print("A. Son datos continuos")
+
+
+clases, limsInf, limsSup, mrksClases, fa, fr, frAc = digCont.generateGroupedData(datosEj12,2,6)
+frStr, frAcStr = tbl.datosStrPorcentaje(fr, frAc)
+print(sum(fa))
+
+encabezados = ["Clases", "Límites inferiores", "Límites superiores", "Marca de clase", "Fa", "Fr", "FrAcum."]
+contenido = [clases, limsInf, limsSup, mrksClases, fa, frStr, frAcStr]
+tbl.printHTMLTable(encabezados, contenido)
+
+
+graf.createHistogram(mrksClases, fr) 
+graf.createOjiva(mrksClases, frAc)
